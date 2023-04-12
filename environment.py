a21cbe9b6e1100618740461ab98e1a6797609909
melihteke <36086368+melihteke@users.noreply.github.com>
2023-04-12 15:26:01 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -8,8 +8,5 @@ from dotenv import load_dotenv
 dotenv_current_path = os.path.join(pathlib.Path().resolve(), '.env')
 load_dotenv(dotenv_current_path)
 
-DBA_USR = os.environ.get("DBA_USR")
-DBA_PSSWD = os.environ.get("DBA_PSSWD")
-DBA_URL = os.environ.get("DBA_URL")
 DBA_URI = os.environ.get("DBA_URI")
 
